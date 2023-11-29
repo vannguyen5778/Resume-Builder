0901840eeefe41ff1f13c5a898e4d5328131dce7
vannguyen5778 <hoai.van@bk.ru>
2023-11-29 16:55:33 +0700

    "First commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-
-import Navbar from './components/Navbar'
-import './assets/styles/App.scss'
-import Home from './pages/Home'
+import Navbar from "./components/Navbar";
+import "./assets/styles/App.scss";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <div className="app">
       <Navbar />
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// cool animations
+// redux
+// context api
+// scss modules
+// drag and drop
+// authentication
+// validation
+// data from backend
+// react router
+// motion div
+
+// pagination
+// fetch data
+// cart logic
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import logo1 from '../assets/images/logo1.svg'
-import logo2 from '../assets/images/logo20.svg'
-import logo3 from '../assets/images/logo21.svg'
-import logo4 from '../assets/images/logo11.svg'
-import logo5 from '../assets/images/logo6.png'
+import logo1 from "../assets/images/logo1.svg";
+import logo2 from "../assets/images/logo20.svg";
+import logo3 from "../assets/images/logo21.svg";
+import logo4 from "../assets/images/logo11.svg";
+import logo5 from "../assets/images/logo6.png";
 const Home = () => {
   return (
     <div className="home">
@@ -30,6 +30,43 @@ const Home = () => {
         <img src={logo4} alt="" />
         <img src={logo2} alt="" />
         <img src={logo3} alt="" />
+      </div>
+      <div className="reviews-ctn">
+        <h2>Reviewed by the community. Trusted by professionals</h2>
+        <div className="overall">
+          <p>4.5 out of 5</p>
+          <img src="" alt="" className="stars" />
+          <p className="source"></p>
+          <p>based on 47,905 reviews</p>
+        </div>
+        <div className="detailed">
+          <div className="individual">
+            <div className="review">
+              <img
+                src="https://resume.io/assets/landing/home/star-green-165f651514ac48ee6e7f5a3bcabec39097349b3150df38ff4082dcbf3be79410.svg"
+                alt=""
+                className="stars"
+              />
+              <h3 className="short-desc">Very easy to use</h3>
+              <p className="long-desc">
+                A very easy to use and intuitive platform to create a high
+                quality CV. The additional CV review makes a great impact in
+                composing a successful CV that puts you on top of other
+                applicants.
+              </p>
+              <div className="bottom">
+                <p className="name">Mike Alexandru</p>
+                <span></span>
+                <p className="time">about 1 hour ago</p>
+              </div>
+            </div>
+          </div>
+          <div className="scroll">
+            <span className="back"></span>
+            <span className="forward"></span>
+            <span className="bar"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
