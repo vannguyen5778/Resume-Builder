481b886a4cf44789d15bbe4b32360d17a0523cff
vannguyen5778 <hoai.van@bk.ru>
2023-12-13 12:38:11 +0700

    Add convertUnixToDate helper function

--- src/redux/resumeSlice.js
+++ src/redux/resumeSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { convertUnixtoDate } from "../utils/convertUnixToDate";
 // const formsTitle = [
 //   "Education",
 //   "Experience",
@@ -197,125 +197,22 @@ const emptyForm = [
     ],
   },
 ];
+const currentTime = new Date().getTime();
 
 const initialState = {
   resumes: [
     {
-      id: 1,
-      name: "Front-end dev",
-      resume: [
-        { id: 0, content: "Education", subItems: [{ time: "24 dec" }] },
-        { id: 1, content: "Experience", subItems: [{ time: "24 dec" }] },
-        { id: 2, content: "Education", subItems: [{ time: "24 dec" }] },
-      ],
-    },
+      id: new Date().getTime(),
+      title: "", 
+      lastUpdate: convertUnixtoDate(new Date().getTime()),
+      img: "",
+      data: defaultForm,
+
+    }
   ],
   resume: [],
 
-  forms: [
-    {
-      id: 1,
-      content: "Education",
-      subItems: [
-        {
-          id: 10,
-          content: "Northeastern University",
-          start: "Sep. ’17",
-          end: "Dec. ’21",
-          degree:
-            "Bachelor of Science in Computer Science, Minor in Professional Presentation",
-          description:
-            "GPA: 3.99 / 4.00. Dean’s scholarship\nCourses: Computer Systems, Networks, Prob. and Stat., Object Oriented Design, Algorithms, Artificial Intelligence\nTeaching Assistant: Discrete Structures (provided once hours, exam reviews, grading), Professional Development for Co-op (provided resume reviews, interview prep, mentorship)",
-          isExpanded: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      content: "Experience",
-      subItems: [
-        {
-          id: 20,
-          content: "Facebook",
-          position: "Software Engineering Intern",
-          start: "Jan. ’21",
-          end: "Apr. ’21",
-          description:
-            "Replaced script-based e-commerce order platform (that handles thousands of daily orders) with a state machine, leading to an 88% decrease in order processing time\nEnabled orders to automatically resolve from certain broken states which previously required manual resolution",
-        },
-        {
-          id: 21,
-          content: "Alignable",
-          position: "Software Engineering Co-op",
-          start: "May ’18",
-          end: "Mar. ’20",
-          description:
-            "Increased forum email click-to-open rate from 22.2% to 65.3% by building aggregations into email infrastructure\nBuilt new comments section for the forum adding nested comments and pagination while supporting conditionally hiding posts for certain users (shadowbanning)",
-        },
-      ],
-    },
-    {
-      id: 3,
-      content: "Skills",
-      subItems: [
-        {
-          id: 10,
-          content:
-            "Python, Java, C/C++ (familiar), Ruby on Rails, PHP/Hack, Javascript, React, Rust (familiar), Poker",
-        },
-      ],
-    },
-    {
-      id: 4,
-      content: "Projects",
-      subItems: [
-        {
-          id: 1045,
-          content: "Sudoku Solver",
-          description:
-            "Parallel, memoized, low allocation DFS solver beating sudoku puzzles in 3ms",
-          tech: "Rust",
-          start: "Jan. ’20",
-          end: "Mar. ’20",
-          link: "github.com/sudoku.34354",
-        },
-        {
-          id: 1045454,
-          content: "Ray Tracer",
-          description:
-            "Parallel ray tracer implementing anti-aliasing, diuse/metal/glass materials, and a movable camera",
-          tech: "Rust",
-          start: "Jun. ’20",
-          end: "Nov. ’21",
-          link: "github.com/ray.3t54",
-        },
-      ],
-    },
-    {
-      id: 245345,
-      content: "Personal Details",
-      subItems: [
-        {
-          id: 20,
-          email: "simon.wh@dev.com",
-          phone: "508-505-6480",
-          address: "NYC, US",
-          fullName: "Simon White",
-        },
-      ],
-    },
-    {
-      id: 934873294723,
-      content: "Certificates",
-      subItems: [
-        {
-          id: 3432432434,
-          content: "IELTS",
-          level: "8.0",
-        },
-      ],
-    },
-  ],
+  forms: defaultForm,
 };
 
 export const resumes = createSlice({
@@ -395,7 +292,6 @@ export const resumes = createSlice({
       const form = state.forms.find((form) => form.id === Number(formId));
       const subItem = form.subItems.find((subItem) => subItemId === subItem.id);
       subItem.isExpanded = isExpandedState;
-      console.log(subItem.isExpanded);
     },
 
     handleInputChange(state, action) {
---
+++ src/utils/convertUnixToDate.js
@@ -0,0 +1,26 @@
+const monthsNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const convertUnixtoDate = (unix) => {
+  const date = new Date(unix);
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return `${day} ${
+    monthsNames[monthIndex]
+  } ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+};
