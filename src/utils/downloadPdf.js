509f7caa8d21d827d095c3d55d6610efb434d148
vannguyen5778 <hoai.van@bk.ru>
2023-12-14 13:14:57 +0700

    Split downloadPdf function into captureImg and generatePdf

--- src/utils/downloadPdf.js
+++ src/utils/downloadPdf.js
@@ -1,22 +1,49 @@
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 
-const downloadPdf = (rootElementId, downloadFileName) => {
-    const input = document.getElementById(rootElementId);
-    const width = input.offsetWidth;
-    const height = input.offsetHeight;
-  
-    html2canvas(input, { scale: 1 }).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "mm", [width, height]);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-      const x = (pdfWidth - width) / 2;
-      const y = (pdfHeight - height) / 2;
-      pdf.addImage(imgData, "PNG", x, y, width, height);
-      pdf.save(`${downloadFileName}.pdf`);
-    });
+export const captureImage = (rootElementId) => {
+  const input = document.getElementById(rootElementId);
+  return html2canvas(input, { scale: 1 }).then((canvas) => {
+    return canvas.toDataURL("image/png");
+  });
+};
+
+const generatePdf = (imageData, downloadFileName) => {
+  const pdf = new jsPDF("p", "mm");
+  const pdfWidth = pdf.internal.pageSize.getWidth();
+  const pdfHeight = pdf.internal.pageSize.getHeight();
+  const img = new Image();
+
+  img.onload = function () {
+    const imageWidth = img.width;
+    const imageHeight = img.height;
+    const aspectRatio = imageWidth / imageHeight;
+
+    let targetWidth = pdfWidth;
+    let targetHeight = pdfWidth / aspectRatio;
+
+    if (targetHeight > pdfHeight) {
+      targetHeight = pdfHeight;
+      targetWidth = pdfHeight * aspectRatio;
+    }
+
+    const x = (pdfWidth - targetWidth) / 2;
+    const y = (pdfHeight - targetHeight) / 2;
+
+    pdf.addImage(imageData, "PNG", x, y, targetWidth, targetHeight);
+    pdf.save(`${downloadFileName}.pdf`);
   };
+
+  img.src = imageData;
+};
+
+const downloadPdf = (rootElementId, downloadFileName) => {
+  captureImage(rootElementId).then((imageData) => {
+    generatePdf(imageData, downloadFileName);
+  });
+};
+
+
   
 
 export default downloadPdf;
