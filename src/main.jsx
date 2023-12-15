d73751441b210c76de63706c848e06e2eb44b983
vannguyen5778 <hoai.van@bk.ru>
2023-12-15 16:22:36 +0700

    Add react-persist to store states

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { store } from "../src/redux/store.js"
+import { store, persistor } from "../src/redux/store.js"
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </ Provider>
   </React.StrictMode>
   </BrowserRouter>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
-import resumes  from './resumeSlice'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import resumes from "./resumeSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ resumes });
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {resumes},
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
