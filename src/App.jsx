01ebf37f66bfe7c03896c25a05009c185de852e7
vannguyen5778 <hoai.van@bk.ru>
2023-12-18 15:08:50 +0700

    Resolve react prop warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./assets/styles/App.scss";
+import Home from "./pages/home";
 import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
-const Home = lazy(() => import("./pages/home/index.jsx"));
 const Resumes = lazy(() => import("./pages/resumes"));
 const Resume = lazy(() => import("./pages/resume"));
 
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="app">
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/resumes" element={<Resumes />} />
-        <Route path="/resume/:id" element={<Resume />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/resumes" element={<Resumes />} />
+          <Route path="/resume/:id" element={<Resume />} />
+        </Routes>
       </Suspense>
     </div>
   );
--- src/pages/home/Animations.jsx
+++ src/pages/home/Animations.jsx
@@ -7,19 +7,19 @@ const CardAnimation = () => {
     height: 200px;
     transform-style: preserve-3d;
     animation-delay: ${(Math.floor(Math.random() * 10) + 1) * 1000}ms;
-    animation: ${({ initialX }) => fallingAnimation(initialX)} 10s linear
+    animation: ${({ initial }) => fallingAnimation(initial)} 10s linear
       infinite forwards;
   `;
 
-  const fallingAnimation = (initialX) => keyframes`
+  const fallingAnimation = (initial) => keyframes`
     0% {
       transform: translate(${
-        initialX * 400 + 50
+        initial * 400 + 50
       }px, -550px) rotateY(0deg);
     }        
     100% {
       transform: translate(${
-        initialX * 400  + 50
+        initial * 400  + 50
       }px, 200px) rotateY(360deg);
     }
   `;
@@ -27,7 +27,7 @@ const CardAnimation = () => {
   return (
     <>
       {[...Array(4)].map((_, index) => (
-        <Card initialX={index} index={index} key={index}>
+        <Card initial={index} index={index} key={index}>
           <div className="front">
             <img
               src="https://resume.io/assets/media/skeleton-190b2a1e1dbef7a47ffdc.png"
